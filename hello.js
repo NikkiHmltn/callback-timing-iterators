aaf3d35ecd5593b10f8fb23fff783889424ced72
Nicole Hamilton <Nicole@Warps-MacBook-Air.local>
2020-10-22 07:06:34 -0700

    [js] add 5 more sayHello functions

--- hello.js
+++ hello.js
@@ -27,3 +27,33 @@ function sayHello(name) {
      return msg;
  }
  console.log(sayHello('class'));
+
+function sayHello(name) {
+    let msg = "Hello " +name+ ". How are you?";
+    return msg;
+}
+console.log(sayHello('Nikki'));
+
+function sayHello(name) {
+    let msg= "Hi " +name+ ". How are you?";
+    return msg;
+}
+console.log(sayHello('Howard'));
+
+function sayHello(name) {
+    let msg = "Yo " +name+ ". How are you today?";
+    return msg;
+}
+console.log(sayHello('tony'));
+
+function sayHello(name) {
+    let msg = "Hi " +name+ ". Good day.";
+    return msg;
+}
+console.log(sayHello('Linda'));
+
+function sayHello(name) {
+    let msg = "Yo " +name+ ". Hi";
+    return msg;
+}
+console.log(sayHello('Tanner'));
