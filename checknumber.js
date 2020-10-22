f8eddb5d940a7f7a01ec4bc9ee68f8369b649a88
Nicole Hamilton <Nicole@Warps-MacBook-Air.local>
2020-10-22 07:18:22 -0700

    [js] add like 2 more functions. not 10 total

--- checknumber.js
+++ checknumber.js
@@ -52,3 +52,31 @@ function checkNumber(num) {
     }
 }
 console.log(checkNumber(1));
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative' 
+    } else {
+        return 'zero';
+    }
+}
+console.log(checkNumber(4));
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+console.log(checkNumber(-12));  
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0)
+ }
