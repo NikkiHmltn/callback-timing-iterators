e380912782b52712e9c4f0015ea9a1e28f6a7a83
Nicole Hamilton <Nicole@Warps-MacBook-Air.local>
2020-10-22 07:13:31 -0700

    [checknum] add 5 functions

---
+++ checknumber.js
@@ -0,0 +1,54 @@
+function checkNumber(num) {
+    if (num >0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+console.log(checkNumber(5));
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+console.log(checkNumber(0)); 
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+console.log(checkNumber(-22));
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    }else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+console.log(checkNumber(20));
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive' 
+    } else if (num <0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+console.log(checkNumber(1));
